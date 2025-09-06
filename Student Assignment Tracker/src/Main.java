2337cdee66f2d480e9a0d3bb107a684e5a46b864
khat0076 <khattab2003@live.com>
2025-09-06 19:54:01 +0200

    Update Main.java

--- Student Assignment Tracker/src/Main.java
+++ Student Assignment Tracker/src/Main.java
@@ -25,12 +25,33 @@ public class Main {
 
 
 
+        //Asking user to input the student name
+        System.out.print("Enter Student Name: ");
+        String studentName = scanner.nextLine();
+
+        //A while loop to make sure that the teacher does not input an empty student
+        while (studentName.trim().isEmpty()){
+            System.out.print("Enter a valid student name: ");
+            studentName = scanner.nextLine();
+        }
+        //
+        System.out.println(studentName + " have been added to the system!");
+
+
+        //Asking the user to input the ID of the student they wrote in studentName
+        System.out.print("Enter student ID: ");
+        int studentID = scanner.nextInt();
+        //To remove the new line
+        scanner.nextLine();
+
+        //Student object, takes the user input student name and ID as its arguments
+        Student student1 = new Student(studentName, studentID);
 
 
 
 
 
-        
+
 
 
 
