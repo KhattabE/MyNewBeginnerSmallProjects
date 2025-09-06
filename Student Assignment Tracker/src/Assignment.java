a2e0658b3f3620c3223e74706c253ed6e20bcfd1
khat0076 <khattab2003@live.com>
2025-09-06 19:14:34 +0200

    update Assignment.java

---
+++ Student Assignment Tracker/src/Assignment.java
@@ -0,0 +1,61 @@
+public class Assignment {
+
+    //Assignment class Variables
+    private String title;
+    private String subject;
+    private String dueDate;
+    private boolean isCompleted;
+
+
+    //Class constructor
+    Assignment(String title, String subject, String dueDate, boolean isCompleted) {
+        this.title = title;
+        this.subject = subject;
+        this.dueDate = dueDate;
+        this.isCompleted = isCompleted;
+    }
+
+    //Getter method to make the field readable
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getSubject(){
+        return this.subject;
+    }
+
+    public String getDueDate(){
+        return this.dueDate;
+    }
+
+    public boolean getIsCompleted(){
+        return this.isCompleted;
+    }
+
+    //Setter method to make the field writable
+    void setTitle(String title){
+        this.title = title;
+    }
+
+    void setSubject(String subject){
+        this.subject = subject;
+    }
+
+    void setDueDate(String dueDate){
+        this.dueDate = dueDate;
+    }
+
+    void setCompleted(boolean isCompleted){
+        this.isCompleted = isCompleted;
+    }
+
+    String displayAssignmentInfo(){
+        return "Assignment title: " + this.title + "\nAssignment subject: " + this.subject +
+                "\nAssignment submission date: " + this.dueDate +
+                "\nAssignment completed?: " + isCompleted;
+
+
+    }
+
+
+}
---
+++ Student Assignment Tracker/src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+
+
+    public static void main(String[] args){
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
