dc7c6e527df5c8c85fd9dfe6ad650313de29935f
khat0076 <khattab2003@live.com>
2025-09-09 12:46:03 +0200

    Update Vehicle.java

--- Garage Management System/src/Vehicle.java
+++ Garage Management System/src/Vehicle.java
@@ -4,24 +4,60 @@ public class Vehicle {
     private String brand;
     private String licensPlate;
     private String vehicleColor;
-    boolean isCarElectric;
+    boolean isVehicleElectric;
 
 
     //Vehicle class constructor
-    Vehicle(String brand, String licensPlate, String vehicleColor, boolean isCarElectric){
+    Vehicle(String brand, String licensPlate, String vehicleColor, boolean isVehicleElectric) {
         this.brand = brand;
         this.licensPlate = licensPlate;
         this.vehicleColor = vehicleColor;
-        this.isCarElectric = isCarElectric;
+        this.isVehicleElectric = isVehicleElectric;
 
 
     }
 
 
-    
+    //Getter method to make the code readable
+    public String getBrand() {
+        return this.brand = brand;
+    }
 
+    public String getLicensPlate() {
+        return this.licensPlate = licensPlate;
+    }
 
+    public String getVehicleColor() {
+        return this.vehicleColor = vehicleColor;
+    }
+    public boolean isVehicleElectric(){
+        return this.isVehicleElectric = isVehicleElectric;
+    }
 
+    //Setter method to make the code writeable
+    public void setBrand(String brand){
+            this.brand = brand;
+    }
+
+    public void setLicensPlate(String licensPlate){
+        this.licensPlate = licensPlate;
+    }
+
+    public void setVehicleColor(String vehicleColor){
+        this.vehicleColor = vehicleColor;
+    }
+
+    public void isVehicleElectric(boolean isVehicleElectric){
+        this.isVehicleElectric = isVehicleElectric;
+    }
+
+    void displayInfoAboutVehicle(){
+        System.out.println("Vehicle brand: " + this.brand +
+                "\nVehicle License Plate: " + this.licensPlate +
+                "\nVehicle Color: " + this.vehicleColor +
+                "\nIs vehicle electric?: " + this.isVehicleElectric);
+
+    }
 
 
 }
