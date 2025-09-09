729b7a3a780634f84a145e180b8e9b1650bf71a8
khat0076 <khattab2003@live.com>
2025-09-09 13:16:30 +0200

    Created Truck.java

--- Garage Management System/src/Truck.java
+++ Garage Management System/src/Truck.java
@@ -4,7 +4,35 @@ public class Truck extends Vehicle{
     public double maxLoadCapacity;
 
     //Child class(Truck) constructor
-    Truck(String brand, String licensPlate, String vehicleColor, boolean isVehicleElectric, double maxLoadCapacity)
+    Truck(String brand, String licensPlate, String vehicleColor, boolean isVehicleElectric, double maxLoadCapacity){
+
+        //Super keyword to get the parent class constructor details
+        super(brand, licensPlate, vehicleColor, isVehicleElectric);
+
+        this.maxLoadCapacity = maxLoadCapacity;
+    }
+
+
+    //Getter method to make the field readable
+    public double getMaxLoadCapacity(){
+        return this.maxLoadCapacity;
+    }
+
+    //Setter method to make the field writable
+    public void setMaxLoadCapacity(double maxLoadCapacity){
+        this.maxLoadCapacity = maxLoadCapacity;
+    }
+
+    //Override the parent class displayInfoABoutVehicle method, to add extra info specified to this child class
+    @Override
+    void displayInfoAboutVehicle() {
+        System.out.println("Vehicle brand: " + this.getBrand() +
+                "\nVehicle License Plate: " + this.getLicensPlate() +
+                "\nVehicle Color: " + this.getVehicleColor() +
+                "\nIs vehicle electric?: " + this.isVehicleElectric +
+                "\nMax Load Capacity: " + this.maxLoadCapacity);
+
+    }
 
 
 }
