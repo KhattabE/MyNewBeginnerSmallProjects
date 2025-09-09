fbf06fb5bc2e9655a36085ca94635cdbd9800a1d
khat0076 <khattab2003@live.com>
2025-09-09 12:34:31 +0200

    Created the parent class Vehicle.java

---
+++ Garage Management System/src/Vehicle.java
@@ -0,0 +1,27 @@
+public class Vehicle {
+
+    //Class fields(Variables)
+    private String brand;
+    private String licensPlate;
+    private String vehicleColor;
+    boolean isCarElectric;
+
+
+    //Vehicle class constructor
+    Vehicle(String brand, String licensPlate, String vehicleColor, boolean isCarElectric){
+        this.brand = brand;
+        this.licensPlate = licensPlate;
+        this.vehicleColor = vehicleColor;
+        this.isCarElectric = isCarElectric;
+
+
+    }
+
+
+    
+
+
+
+
+
+}
